2da6561919e2396dc6e6a9fcae6b99941112a6e2
Bendy545 <chmelik@spsejecna.cz>
2025-02-17 14:15:25 +0100

    added passwordChecker

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import PasswordInput from "./PasswordInput.tsx";
+import PasswordStrength from "./PasswordStrength.tsx";
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+    const [count, setCount] = useState(0)
+    const [password, setPassword] = useState('');
   return (
     <>
       <div>
@@ -28,6 +33,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+        <div style={{ padding: '20px' }}>
+            <PasswordInput setPassword={setPassword} />
+            <PasswordStrength password={password} />
+        </div>
     </>
   )
 }
