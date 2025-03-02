f53b1b3a6e9c4e34b0abab82c677c72a4532606c
Bendy545 <chmelik@spsejecna.cz>
2025-03-02 14:13:05 +0100

    new classes for CurrentTemperature.tsx etc.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,35 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
 import './App.css'
 import PasswordInput2 from "./PasswordInput2.tsx";
 import PasswordStrength from "./PasswordStrength.tsx";
+import CharacterSequenceValidator from "./CharacterSequenceValidator.tsx";
+import PasswordTimeValidator from "./PasswordTimeValidator.tsx";
+import CurrentTemperature from "./CurrentTemperature.tsx";
 
 
 
 
 function App() {
     const [password, setPassword] = useState<string | null>(null);
+    const [passwordTime, setPasswordTime] = useState<number>(Date.now());
+    const minLength = 4;
+
+
+    useEffect(() => {
+        if (password !== null) {
+            setPasswordTime(Date.now());
+        }
+    }, [password]);
+
+
 
   return (
 <>
       <PasswordInput2 passwordValue={password} setter={setPassword} />
       <PasswordStrength password={password}/>
+    <CharacterSequenceValidator password={password} minLength={minLength}/>
+    <PasswordTimeValidator password={password} time={passwordTime}/>
+    <CurrentTemperature/>
 </>
   )
 }
